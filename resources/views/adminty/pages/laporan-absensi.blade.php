1ff117ecb0ef3393159314ec40478c81044637c7
Aldy Rifaldi.B <aldyrifaldi060621@gmail.com>
2020-03-06 21:45:43 +0700

    search student

--- resources/views/adminty/pages/laporan-absensi.blade.php
+++ resources/views/adminty/pages/laporan-absensi.blade.php
@@ -239,6 +239,39 @@
         }
 
 
+        $("#cari_santri").keyup(function () {
+            //split the current value of searchInput
+            var data = this.value.split(" ");
+            //create a jquery object of the rows
+            var jo = $("#daftar_santri").find("tr");
+            if (this.value == "") {
+                jo.show();
+                return;
+            }
+            //hide all the rows
+            jo.hide();
+
+            //Recusively filter the jquery object to get results.
+            jo.filter(function (i, v) {
+                var $t = $(this);
+                for (var d = 0; d < data.length; ++d) {
+                    if ($t.is(":contains('" + data[d] + "')")) {
+                        return true;
+                    }
+                }
+                return false;
+            })
+            //show the rows that match.
+            .show();
+        }).focus(function () {
+            this.value = "";
+            $(this).css({
+                "color": "black"
+            });
+            $(this).unbind('focus');
+        }).css({
+            "color": "#C0C0C0"
+        });
 
         function kelas(){
             var kelas = $('#id_kelas').val();
